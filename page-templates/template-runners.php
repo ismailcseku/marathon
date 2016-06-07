0b6dfd4e92e757ed6b3869f3a41b656791e6e411
hubkodesolution <info.kodesolution@gmail.com>
2016-06-07 12:01:44 +0600

    separated runners by two cat

--- page-templates/template-runners.php
+++ page-templates/template-runners.php
@@ -14,9 +14,16 @@ get_header(); ?>
     <div class="section-content">
       <div class="row">
       
-      
+<h2>Berlin Marathon Runners</h2>
 <?php
-	$runners = get_users();
+$args = array(
+	'meta_key'     => 'what_marathon_like_to_participate',
+	'meta_value'   => 'Berlin Marathon',
+	'meta_compare' => '=',
+ ); 
+?>
+<?php
+	$runners = get_users($args);
 	// Array of stdClass objects.
 	foreach ( $runners as $user ) {
 		$user_id = $user->ID;
@@ -55,7 +62,55 @@ get_header(); ?>
         </div>
 <?php } ?>
 
-            
+
+<div class="clearfix"></div>
+<h2>Jerusalem Marathon runners</h2>
+<?php
+$args = array(
+	'meta_key'     => 'what_marathon_like_to_participate',
+	'meta_value'   => 'Jerusalem Marathon',
+	'meta_compare' => '=',
+ ); 
+?>
+<?php
+	$runners = get_users($args);
+	// Array of stdClass objects.
+	foreach ( $runners as $user ) {
+		$user_id = $user->ID;
+		$allowed_to_get_fund = get_user_meta($user_id, 'allowed_to_get_fund', true);
+		if( $allowed_to_get_fund != true ) {
+			continue;
+		}
+		$runner_funding_goal = get_user_meta($user_id, 'runner_funding_goal', true);
+		if($runner_funding_goal=="") {
+			$runner_funding_goal = 0;
+		}
+		$runner_profile_photo = get_user_meta($user_id, 'runner_profile_photo', true);
+		if($runner_profile_photo=="") {
+			$runner_profile_photo = get_template_directory_uri()."/images/no-photo.jpg";
+		}
+?>
+
+        <div class="col-xs-12 col-sm-6 col-md-4 mb-10">
+          <div class="row equal-height mr-0">
+            <div class="col-xs-6 p-0">
+              <div class="visit-details text-center">
+                <h3 class="text-white"><?php  echo esc_html( $user->display_name ); ?></h3>
+                <div class="">
+                  <a data-bg-color="#1f9b22" class="btn font-13 text-white" href="<?php echo esc_url( home_url('/runner-page/?r='.$user_id) ); ?>">Visit My Page</a>
+                </div>
+                <div class="price">
+                  My goal: $<?php  echo number_format($runner_funding_goal); ?>
+                </div>
+              </div>
+            </div>
+            <div class="col-xs-6 p-0 runner-bg-img" style="background:url('<?php echo $runner_profile_photo?>') center;">
+              <div class="visit-thumb">
+              </div>
+            </div>
+          </div>
+        </div>
+<?php } ?>  
         
       </div>
     </div>
